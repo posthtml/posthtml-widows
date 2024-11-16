bd99ff9fe794eff509569948dfe46aa2e9f49fb6
Cosmin Popovici <hellocosmin@gmail.com>
2024-11-16 03:59:59 +0200

    test: add more tests

--- test/test.js
+++ test/test.js
@@ -1,26 +1,57 @@
-import path from 'node:path'
-import {readFileSync} from 'node:fs'
-import {fileURLToPath} from 'node:url'
 import {test, expect} from 'vitest'
 import posthtml from 'posthtml'
 import plugin from '../lib/index.js'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
-const fixture = file => readFileSync(path.join(__dirname, 'fixtures', `${file}.html`), 'utf8').trim()
-const expected = file => readFileSync(path.join(__dirname, 'expected', `${file}.html`), 'utf8').trim()
-
-// eslint-disable-next-line
-const error = (name, options, cb) => posthtml([plugin(options)]).process(fixture(name)).catch(cb)
 const clean = html => html.replaceAll(/[^\S\r\n]+$/gm, '').trim()
 
-const process = (name, options, log = false) => {
+const process = (input, options, log = false) => {
   return posthtml([plugin(options)])
-    .process(fixture(name))
-    .then(result => log ? console.log(result.html) : clean(result.html)) // eslint-disable-line
-    .then(html => expect(html).toEqual(expected(name)))
+    .process(input)
+    .then(result => log ? console.log(result.html) : clean(result.html))
+    .then(html => html)
 }
 
-test('Basic', () => {
-  return process('basic')
+test('Skips unmarked nodes', async () => {
+  expect(await process('skips unmarked strings')).toEqual('skips unmarked strings')
+  expect(await process('<p>skips unmarked tags</p>')).toEqual('<p>skips unmarked tags</p>')
+})
+
+test('Basic functionality', async () => {
+  expect(
+    await process('<p prevent-widows>lorem ipsum dolor</p>')
+  ).toEqual('<p>lorem ipsum&nbsp;dolor</p>')
+})
+
+test('Nested tags', async () => {
+  expect(
+    await process('<p prevent-widows><span>lorem ipsum dolor</span></p>')
+  ).toEqual('<p><span>lorem ipsum&nbsp;dolor</span></p>')
+})
+
+test('Mixed content', async () => {
+  expect(
+    await process('<p prevent-widows>lorem ipsum <span>dolor</span></p>')
+  ).toEqual('<p>lorem ipsum <span>dolor</span></p>')
+
+  expect(
+    await process('<p prevent-widows>lorem ipsum <span>dolor sit amet</span></p>')
+  ).toEqual('<p>lorem ipsum <span>dolor sit&nbsp;amet</span></p>')
+})
+
+test('Empty content', async () => {
+  expect(
+    await process('<p prevent-widows></p>')
+  ).toEqual('<p></p>')
+})
+
+test('Multiple nested levels', async () => {
+  expect(
+    await process('<div prevent-widows><p>one two</p><p>three four</p></div>')
+  ).toEqual('<div><p>one&nbsp;two</p><p>three&nbsp;four</p></div>')
+})
+
+test('Attribute in nested tags only', async () => {
+  expect(
+    await process('<p><span prevent-widows>ipsum dolor</span></p>')
+  ).toEqual('<p><span>ipsum&nbsp;dolor</span></p>')
 })
